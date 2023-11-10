d06b81f8f552a75f668b165e94ed2194add9d48a
Manishcodelive <manishmaurya13678@gmail.com>
2023-11-10 09:54:28 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function showimg(element){
+    if( element.style.width="6%"){
+        document.querySelector('.card1').style.width="6%"
+        document.querySelector('.card2').style.width="6%"
+        document.querySelector('.card3').style.width="6%"
+        document.querySelector('.card4').style.width="6%"
+        document.querySelector('.card5').style.width="6%"
+        element.style.width="50%";
+       }
+       else{
+        document.querySelector('.card1').style.width="6%"
+        document.querySelector('.card2').style.width="6%"
+        document.querySelector('.card3').style.width="6%"
+        document.querySelector('.card4').style.width="6%"
+        document.querySelector('.card5').style.width="6%"
+       }
+}
